2353fe4b3c254698122b222aa6f19537cce73a78
MadihaPervin <madihapervin068@gmail.com>
2024-07-04 21:09:36 +0600

    first commit

---
+++ June11.js
@@ -0,0 +1,26 @@
+//console.log("JS File is now connected");
+var person1;
+person1 = "FirstName","LastName";
+
+var person2 = "ABC Ahmed";
+
+var grade = "8";
+
+var number1 = 100;
+var cgpa = 2.75;
+var cgpa = 3.99;
+var canVote = ture;
+
+//very very v v v v imp type of variables - array, object
+
+console.log ("Hello ", person2, "-Your cgpa is:"+ cgpa);
+
+prompt("Where you will be during the coming EID vacation?");
+
+var studentName = prompt ("Please Enter Name");
+var number1 = prompt ("Enter 1st Number");
+var number2 = prompt ("Enter 2nd Number");
+var result = number1 + number2;
+console.log ("Hello", studentName, "sum of 2 numbers are: ", result); 
+
+
---
+++ june11-if.js
@@ -0,0 +1,13 @@
+//console.log ("welcome");
+var userName = promp ("Please Enter Name");
+var age = parseInt (prompt("Please enter age"));
+
+if (age >=18){
+    console.log ("Hello", userName, "You can vote this time");
+
+}
+
+else {
+    console.log ("Hello", userName,", sorry - you cant vote thsi time");
+
+}
